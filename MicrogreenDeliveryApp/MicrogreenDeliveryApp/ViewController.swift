3691fc8ff2ca94fce422005170742bb696545e97
Worstling <vladrommel4@gmail.com>
2024-02-10 22:12:06 +0400

    Created base project, removed storyboard references

---
+++ MicrogreenDeliveryApp/MicrogreenDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MicrogreenDeliveryApp
+//
+//  Created by Vladislav on 10.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
