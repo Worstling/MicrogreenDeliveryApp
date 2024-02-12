b897d49d9fb56708a2a1d0e96383af879da100e4
Worstling <vladrommel4@gmail.com>
2024-02-13 01:02:59 +0400

    Added colours

---
+++ MicrogreenDeliveryApp/MicrogreenDeliveryApp/Resources/Extensions/UIColor+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Extension.swift
+//  MicrogreenDeliveryApp
+//
+//  Created by Vladislav on 13.02.2024.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let basicBackground = #colorLiteral(red: 0.9411764706, green: 0.9294117647, blue: 0.8862745098, alpha: 1)
+    static let basicGreen = #colorLiteral(red: 0.2509803922, green: 0.7607843137, blue: 0.4235294118, alpha: 1)
+    static let specialBeige = #colorLiteral(red: 0.9019607843, green: 0.8823529412, blue: 0.8196078431, alpha: 1)
+    static let specialMint = #colorLiteral(red: 0.2, green: 0.5529411765, blue: 0.4901960784, alpha: 1)
+    static let specialGreen = #colorLiteral(red: 0.4196078431, green: 0.7215686275, blue: 0.3137254902, alpha: 1)
+    static let specialDarkGreen = #colorLiteral(red: 0.2549019608, green: 0.4901960784, blue: 0.3333333333, alpha: 1)
+    static let specialBlack = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+}
